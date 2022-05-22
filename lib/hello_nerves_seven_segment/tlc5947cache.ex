6a7e5cba8acc09b3360f8702626766dc4cef34b5
Masatoshi N <nishiguchi.masa@gmail.com>
2022-05-22 14:13:03 -0400

    Revert "Invalidate cache"
    This reverts commit fe4a7c79cacfeb15a5f7cba7d95d7ddcf0811a49.

--- lib/hello_nerves_seven_segment/tlc5947cache.ex
+++ lib/hello_nerves_seven_segment/tlc5947cache.ex
@@ -3,8 +3,6 @@ defmodule HelloNervesSevenSegment.TLC5947Cache do
 
   use Agent
 
-  @cache_duration_seconds 60 * 60
-
   def start_link(_opts \\ []) do
     Agent.start_link(fn -> build_initial_state() end, name: __MODULE__)
   end
@@ -27,20 +25,9 @@ defmodule HelloNervesSevenSegment.TLC5947Cache do
 
   defp get_by(params) do
     case Agent.get(__MODULE__, &get_in(&1, [build_cache_key(params)])) do
-      nil ->
-        nil
-
-      cached_value ->
-        if not expired?(cached_value.time), do: cached_value.tlc5947
+      nil -> nil
+      value -> value.tlc5947
     end
-  end
-
-  defp expired?(inserted_at) do
-    DateTime.diff(DateTime.utc_now(), inserted_at) > cache_duration_seconds()
-  end
-
-  defp cache_duration_seconds do
-    @cache_duration_seconds + Enum.random(0..(3 * 60))
   end
 
   defp save(params, tlc5947) do
